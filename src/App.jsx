d6a08488ed63d34ee014cf9e9d5da5cdc34302a3
Ben <121274141+bdaniels8135@users.noreply.github.com>
2024-01-02 22:38:04 -0500

    Create basic page layout plan

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+function App() {
+  return (
+    <>
+      <header>
+        App name?
+        <nav>
+          Nav Elements: customize layout, light/dark mode, download/preview/print pdf
+        </nav>
+      </header>
+      <main>
+        Sections: personal info/summary, contact info, education, experience, skill lists
+      </main>
+    </>
+  )
+}
+
+export default App
